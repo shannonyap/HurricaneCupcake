1e1a588a12ded77623289b916dcf18dc28581b79
sarang <sbhadsavle@tacc.utexas.edu>
2017-04-21 16:02:53 -0500

    Basic working audio visualizer.

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ var ctx;
 
 var musicPlaylist = ["DayByDay.mp3", "SummerTimeMemory.mp3", "DragonfruitSalad.mp3", "MilleFeuille.mp3", "PandaWonder.mp3"];
 var musicCounter = 0;
-var loopMode = 0;
+var repeatTrackMode = 0;
 
 window.onload = function() {
   canvas = document.createElement("canvas");
   canvas.id = "audioVisualizerCanvas";
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight * 0.3;
-  document.body.appendChild(canvas);
+  $('#musicVisualizerDiv').append(canvas);
   ctx = canvas.getContext('2d');
-
+  setupAudioScrubber();
+  setupAudioControls();
   setupWebAudio();
   draw();
 }
@@ -21,7 +22,7 @@ window.onload = function() {
 function setupWebAudio() {
   var audio = document.createElement('audio');
   audio.addEventListener("ended", function(){
-    if (loopMode) {
+    if (repeatTrackMode) {
       audio.currentTime = 0;
       audio.play();
     } else {
@@ -30,10 +31,17 @@ function setupWebAudio() {
       audio.play();
     }
   });
+  setInterval(function(){
+    var progressPercentage = audio.currentTime / audio.duration * 100;
+    var audioHandler = $('#audioSlider').children()[1];
+    var audioProgress = $('#audioSlider').children()[2];
+
+    $(audioHandler).css({left: progressPercentage + "%"});
+    $(audioProgress).css({width: progressPercentage + "%"});
+  }, 10);
   audio.id = 'audioPlayer'
   audio.src = musicPlaylist[musicCounter];
-  audio.controls = 'true';
-  document.body.appendChild(audio);
+  $('#musicVisualizerDiv').append(audio);
   audio.style.width = window.innerWidth * 0.4 + 'px';
 
   var audioContext = new AudioContext();
@@ -41,7 +49,7 @@ function setupWebAudio() {
   var source = audioContext.createMediaElementSource(audio);
   source.connect(analyzer);
   analyzer.connect(audioContext.destination);
-  audio.play();
+  audio.play()
 }
 
 function draw() {
@@ -57,18 +65,59 @@ function draw() {
   }
 }
 
-function restartTrack() {
-  var audio = document.getElementById('audioPlayer');
-  audio.currentTime = 0;
-  audio.load();
-  audio.play();
+function setupAudioScrubber() {
+  var audioBar = $("<div>", {id: "audioBar"});
+  var audioSlider = $("<div>", {id: "audioSlider", class: "ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content"});
+  var slider = $("<div>", {class: "ui-slider-range ui-corner-all ui-widget-header ui-slider-range-min"});
+  slider.css({width: "0%"});
+  var sliderHandle = $("<span>", {tabindex: "0", class: "ui-slider-handle ui-corner-all ui-state-default"});
+  sliderHandle.css({left: "0%"});
+
+  audioSlider.append(slider);
+  audioSlider.append(sliderHandle);
+  audioBar.append(audioSlider);
+
+  $('#musicVisualizerDiv').append(audioBar);
+
+  $("#audioSlider").slider({
+    	min: 0,
+    	max: 100,
+    	value: 0,
+  		range: "min",
+    	slide: function(event, ui) {
+        var audio = document.getElementById('audioPlayer');
+        audio.currentTime = ui.value;
+    	}
+  });
 }
 
-function changeMusic() {
-  nextTrackInPlaylist();
-  var audio = document.getElementById('audioPlayer');
-  audio.src = musicPlaylist[musicCounter];
-  audio.play();
+function setupAudioControls() {
+  var audioControls = $("<div>", {class: "audioControls"});
+  $('#musicVisualizerDiv').append(audioControls);
+
+  var muteTrackButton = createAudioButton('muteTrackButton', 'mute', 'muteTrackInPlaylist()');
+  audioControls.append(muteTrackButton);
+
+  var previousTrackButton = createAudioButton('previousTrackButton', 'backward', 'previousTrackInPlaylist()');
+  audioControls.append(previousTrackButton);
+
+  var playTrackButton = createAudioButton('playTrackButton', 'pause', 'toggleAudio()');
+  audioControls.append(playTrackButton);
+
+  var nextTrackButton = createAudioButton('nextTrackButton', 'forward', 'nextTrackInPlaylist()');
+  audioControls.append(nextTrackButton);
+
+  var repeatTrackButton = createAudioButton('repeatTrackButton', 'repeat', 'repeatTrackInPlaylist()');
+  audioControls.append(repeatTrackButton);
+}
+
+function createAudioButton(id, icon, onclick) {
+  var audioButton = $("<button>", {class: 'ui massive icon button', id: id, onclick: onclick});
+  var audioIcon = $("<i>", {class: icon + ' icon'});
+  audioButton.css({'background-color': 'rgba(0,0,0,0)'});
+  audioButton.append(audioIcon);
+
+  return audioButton;
 }
 
 function nextTrackInPlaylist() {
@@ -76,15 +125,61 @@ function nextTrackInPlaylist() {
   if (musicCounter > musicPlaylist.length - 1) {
     musicCounter = 0;
   }
+  playAudio();
 }
 
-function loop() {
-  var loopButton = document.getElementById('loopButton');
-  if (loopMode == 0) {
-    loopMode = 1;
-    loopButton.innerHTML = "Loop Off";
+function previousTrackInPlaylist() {
+  musicCounter--;
+  if (musicCounter < 0) {
+    musicCounter = musicPlaylist.length - 1;
+  }
+  playAudio();
+}
+
+function playAudio() {
+  var audio = document.getElementById('audioPlayer');
+  audio.src = musicPlaylist[musicCounter];
+  audio.play();
+}
+
+function toggleAudio() {
+  var audio = document.getElementById('audioPlayer');
+  var icon = $('#playTrackButton').children()[0];
+  if (!audio.paused) {
+    // if audio is playing, then pause it
+    $(icon).removeClass('pause');
+    $(icon).addClass('play');
+    audio.pause();
   } else {
-    loopMode = 0;
-    loopButton.innerHTML = "Loop On";
+    $(icon).removeClass('play');
+    $(icon).addClass('pause');
+    audio.play();
+  }
+}
+
+function repeatTrackInPlaylist() {
+  var repeatTrackButton = document.getElementById('repeatTrackButton');
+  var repeatIcon = $(repeatTrackButton).children()[0];
+
+  if (repeatTrackMode == 0) {
+    repeatTrackMode = 1;
+    $(repeatIcon).css({'color': '#5B85AA'});
+  } else {
+    repeatTrackMode = 0;
+    $(repeatIcon).css({'color': '#696562'});
+  }
+}
+
+function muteTrackInPlaylist() {
+  var audio = document.getElementById('audioPlayer');
+  var icon = $('#muteTrackButton').children()[0];
+  if ($(icon).hasClass('mute')) {
+    $(icon).removeClass('mute');
+    $(icon).addClass('unmute');
+    audio.volume = 0;
+  } else {
+    $(icon).removeClass('unmute');
+    $(icon).addClass('mute');
+    audio.volume = 1;
   }
 }
